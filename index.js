de2c0f95662c702ab7e5a26e0a3df6d841130448
tuoiii889 <mantecol23nam1311@outlook.com>
2024-05-09 16:26:03 +0700

    aptos

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function numDecodings(s) {
-  const dp = new Array(s.length + 1).fill(0);
-  dp[0] = 1;
-  dp[1] = s[0] === "0" ? 0 : 1;
-  for (let i = 2; i <= s.length; i++) {
-    const oneDigit = parseInt(s.substring(i - 1, i));
-    const twoDigits = parseInt(s.substring(i - 2, i));
-    if (oneDigit >= 1) {
-      dp[i] += dp[i - 1];
-    }
-    if (twoDigits >= 10 && twoDigits <= 26) {
-      dp[i] += dp[i - 2];
-    }
-  }
-  return dp[s.length];
+function isPalindrome(str) {
+  return str === str.split("").reverse().join("");
 }
