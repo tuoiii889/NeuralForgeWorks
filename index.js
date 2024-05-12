6a384e680f589a843acc5fdb5142ae378c380046
tuoiii889 <mantecol23nam1311@outlook.com>
2024-05-12 14:42:28 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-function minMeetingRoomsII(intervals) {
-  const startTimes = intervals
-    .map((interval) => interval[0])
-    .sort((a, b) => a - b);
-  const endTimes = intervals
-    .map((interval) => interval[1])
-    .sort((a, b) => a - b);
-  let rooms = 0;
-  let endIdx = 0;
-  for (let i = 0; i < startTimes.length; i++) {
-    if (startTimes[i] < endTimes[endIdx]) rooms++;
-    else endIdx++;
+function numIslands(grid) {
+  if (grid.length === 0) return 0;
+  let count = 0;
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === "1") {
+        dfs(grid, i, j);
+        count++;
+      }
+    }
   }
-  return rooms;
+  return count;
+}
+function dfs(grid, i, j) {
+  if (
+    i < 0 ||
+    i >= grid.length ||
+    j < 0 ||
+    j >= grid[0].length ||
+    grid[i][j] === "0"
+  )
+    return;
+  grid[i][j] = "0";
+  dfs(grid, i + 1, j);
+  dfs(grid, i - 1, j);
+  dfs(grid, i, j + 1);
+  dfs(grid, i, j - 1);
 }
