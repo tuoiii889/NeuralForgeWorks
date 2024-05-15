b07a12d6f8eda32de26887e97687d0d726c60c9f
tuoiii889 <mantecol23nam1311@outlook.com>
2024-05-15 19:29:36 +0700

    hello

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function pathSum(root, sum) {
-  if (!root) return false;
-  if (!root.left && !root.right && root.val === sum) return true;
-  return (
-    pathSum(root.left, sum - root.val) || pathSum(root.right, sum - root.val)
-  );
+function canPartition(nums) {
+  const sum = nums.reduce((acc, val) => acc + val, 0);
+  if (sum % 2 !== 0) return false;
+  const target = sum / 2;
+  const dp = new Array(target + 1).fill(false);
+  dp[0] = true;
+  for (const num of nums) {
+    for (let i = target; i >= num; i--) {
+      dp[i] = dp[i] || dp[i - num];
+    }
+  }
+  return dp[target];
 }
